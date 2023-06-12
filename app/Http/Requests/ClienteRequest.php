0cf4012d7ca9e69e235e2f4e3e4bbbd4c1fee59d
Bernardo Norte <bernardonorte3@gmail.com>
2023-06-12 12:39:23 +0100

    commit

--- app/Http/Requests/ClienteRequest.php
+++ app/Http/Requests/ClienteRequest.php
@@ -36,7 +36,27 @@ class ClienteRequest extends FormRequest
             'tipo_pagamento' => 'required|in:MC,PAYPAL,VISA',
             'ref_pagamento' => 'required',
             'foto_url' => 'nullable|image|max:8192',
+            'password_inicial' => 'sometimes|required',
             'bloqueado' => 'required|boolean'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' =>  'O nome é obrigatório',
+            'email.required' => 'O email é obrigatório',
+            'email.email' =>    'O formato do email é inválido',
+            'email.unique' =>   'O email tem que ser único',
+            'nif.required' =>   'O nif é obrigatório',
+            'nif.integer' =>    'O nif tem que ser inteiro',
+            'tipo_pagamento.required' => 'O tipo de pagamento é obrigatório',
+            'tipo_pagamento.in' => 'O tipo de pagamento tem de ser MC, Paypal, Visa',
+            'ref_pagamento.required' => 'A referência de pagamento é obrigatória',
+            'ref_pagamento.char' => 'A referência de pagamento tem de ser com caracteres',
+            'file_foto.image' => 'O ficheiro com a foto não é uma imagem',
+            'file_foto.size' => 'O tamanho do ficheiro com a foto tem que ser inferior a 8 Mb',
+            'password_inicial.required' => 'A password inicial é obrigatória',
         ];
     }
 }
