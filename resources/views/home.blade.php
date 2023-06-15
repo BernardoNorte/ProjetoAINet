8ea57244795fb32ecccf8f8fd0bbb0470c2e8e7a
Bernardo Norte <bernardonorte3@gmail.com>
2023-06-15 14:41:35 +0100

    Resolucao das fotos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,13 @@
 
 
 @section('main')
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Shop in style</h1>
+        </div>
+    </div>
+</header>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
