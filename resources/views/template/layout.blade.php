ac166d00ba7c8a6ffdf28442eb0784625438b83a
Bernardo Norte <bernardonorte3@gmail.com>
2023-06-24 05:18:31 +0100

    alterei na template a clicar no catalogo

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -32,9 +32,6 @@
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="{{ route('catalogo.index') }}">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                             </ul>
                         </li>
                         @if ((Auth::user()->user_type ?? '') == 'C')
